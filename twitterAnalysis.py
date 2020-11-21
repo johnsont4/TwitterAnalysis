87a6c2370833431ee5fd730b15859f08942c7069
Teagan Johnson <teaganjohnson@Teagans-MacBook-Pro.local>
2020-11-21 17:28:52 +0100

    added trump followers

--- twitterAnalysis.py
+++ twitterAnalysis.py
@@ -4,13 +4,14 @@ import twint
 import pandas as pd
 import numpy as np
 from collections import defaultdict
+from collections import Counter
 
-def createFollowersList():
-    with open("/Users/teaganjohnson/Desktop/twint/trumpFollowers.rtf") as file:
+def createFollowersList(path):
+    with open(path) as file:
         markup = file.read()
 
     listOfFollowers = []
-    for name in tqdm(markup.split()[10:]):
+    for name in tqdm(markup.split()[10:20]):
         listOfFollowers.append(name[0:-1])
     return listOfFollowers
 listOfTrumpFollowers = createFollowersList()
@@ -23,9 +24,14 @@ def getFollowing(username):
 
     twint.run.Following(c)
     listOfFollowing = twint.storage.panda.Follow_df
+
     #listOfFollowing = listOfFollowing.reindex(index=listOfFollowing.index[::-1])
     #print(listOfFollowing)
-    return listOfFollowing["following"][username]
+    #listOfFollowing.to_csv(r'/Users/teaganjohnson/Desktop/TwitterFinalProject/.csv', header=None, index=None, sep=' ', mode='a')
+    try:
+        return listOfFollowing["following"][username]
+    except Exception:
+        return ""
 
 def createDictAndList(listOfFollowers):
     followerDict = defaultdict(list)
@@ -34,5 +40,20 @@ def createDictAndList(listOfFollowers):
         listOfFollowers = getFollowing(username)
         totalFollowerList.append(listOfFollowers)
         followerDict['username'] = listOfFollowers
+    return followerDict, totalFollowerList
 
-createDictAndList(listOfTrumpFollowers)
+def getTotalFollowerList(listOfFollowers):
+    #followerDict = defaultdict(list)
+    totalFollowerList = []
+    for username in tqdm(listOfFollowers):
+        listOfFollowers = getFollowing(username)
+        if listOfFollowers != "":
+            for person in listOfFollowers:
+                totalFollowerList.append(person)
+        #followerDict['username'] = listOfFollowers
+    return totalFollowerList
+
+listOfTrumpFollowers = createFollowersList("/Users/teaganjohnson/Desktop/twint/trumpFollowers.rtf")
+totalFollowerList = getTotalFollowerList(listOfTrumpFollowers)
+counterFollowers = Counter(totalFollowerList)
+print(counterFollowers.most_common(10))
