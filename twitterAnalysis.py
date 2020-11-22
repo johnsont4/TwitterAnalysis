26c8ddbc13b14574f0cfb1808c8e480eff7c87ef
Teagan Johnson <teaganjohnson@Teagans-MacBook-Pro.local>
2020-11-22 21:33:26 +0100

    updated, not working properly

--- twitterAnalysis.py
+++ twitterAnalysis.py
@@ -6,16 +6,16 @@ import numpy as np
 from collections import defaultdict
 from collections import Counter
 
-def createFollowersList(path):
+# Opens the text file with user's followers and returns all followers in a list
+def createFollowersList(path, listOfFollowers):
     with open(path) as file:
         markup = file.read()
 
-    listOfFollowers = []
     for name in tqdm(markup.split()[10:20]):
         listOfFollowers.append(name[0:-1])
     return listOfFollowers
-listOfTrumpFollowers = createFollowersList()
 
+# Function that finds who a specific person follows
 def getFollowing(username):
     c = twint.Config()
     c.Username = username
@@ -29,31 +29,36 @@ def getFollowing(username):
     #print(listOfFollowing)
     #listOfFollowing.to_csv(r'/Users/teaganjohnson/Desktop/TwitterFinalProject/.csv', header=None, index=None, sep=' ', mode='a')
     try:
+        print("Works")
+        print()
         return listOfFollowing["following"][username]
     except Exception:
+        print("Doesn't Work")
+        print()
         return ""
 
-def createDictAndList(listOfFollowers):
-    followerDict = defaultdict(list)
-    totalFollowerList = []
-    for username in tqdm(listOfFollowers):
-        listOfFollowers = getFollowing(username)
-        totalFollowerList.append(listOfFollowers)
-        followerDict['username'] = listOfFollowers
-    return followerDict, totalFollowerList
-
+# Function that returns the list of every single follower of a user.
+# It will also include a dictionary where the keys are users and the values
+# are lists of users that they follow.
 def getTotalFollowerList(listOfFollowers):
-    #followerDict = defaultdict(list)
     totalFollowerList = []
     for username in tqdm(listOfFollowers):
-        listOfFollowers = getFollowing(username)
+        try:
+            listOfFollowers = getFollowing(username)
+        except Exception:
+            listOfFollowers = ""
         if listOfFollowers != "":
-            for person in listOfFollowers:
-                totalFollowerList.append(person)
-        #followerDict['username'] = listOfFollowers
+            totalFollowerList.append(listOfFollowers)
     return totalFollowerList
 
-listOfTrumpFollowers = createFollowersList("/Users/teaganjohnson/Desktop/twint/trumpFollowers.rtf")
-totalFollowerList = getTotalFollowerList(listOfTrumpFollowers)
-counterFollowers = Counter(totalFollowerList)
+# Variable that stores list of all followers
+listOfTrumpFollowers = createFollowersList("/Users/teaganjohnson/desktop/trumpFollowers", [])
+
+# Gets a list of who followers are following (It will have duplicates)
+totalFollowerList1 = getTotalFollowerList(listOfTrumpFollowers)
+for x in totalFollowerList1:
+    x = totalFollowerList1
+#print((totalFollowerList))
+# A dictionary that counts the most popular follows in the totalFollowerList
+counterFollowers = Counter(totalFollowerList1)
 print(counterFollowers.most_common(10))
