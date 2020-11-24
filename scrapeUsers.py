b1450c237987a2773428448e8ff3de0240488ff3
Teagan Johnson <teaganjohnson@Teagans-MacBook-Pro.local>
2020-11-25 00:52:54 +0100

    redirected all files to previous folder

--- scrapeUsers.py
+++ scrapeUsers.py
@@ -5,7 +5,7 @@ def getFollowers(username):
     c.Username = username
     c.Limit = 50000
     c.Store_csv = True
-    c.Output = "{}Followers.csv".format(username)
+    c.Output = "../{}Followers.csv".format(username)
 
     twint.run.Followers(c)
 
