4977ff90c8057b0c25949c796e322f4ab63212f9
Teagan Johnson <teaganjohnson@Teagans-MacBook-Pro.local>
2020-11-27 14:34:52 +0100

    added matrices

--- matrixCreator.py
+++ matrixCreator.py
@@ -2,9 +2,12 @@ import numpy as np
 import os
 import pandas as pd
 
-def makeFollowerVector(person, faction, columnsVector):
+currPath = os.getcwd()
+parent = os.path.abspath(os.path.join(currPath, os.pardir))
+
+def makeFollowerVector(person, faction, columnsVector, parent):
     followerVector = []
-    with open("/Users/teaganjohnson/desktop/TwitterAnalysis/{}/{}".format(faction, person)) as user:
+    with open("{}/{}/{}".format(parent, faction, person)) as user:
         dataframe = pd.read_csv(user, usecols=[0])
         dataframe = dataframe.username.tolist()
         for account in columnsVector:
@@ -17,13 +20,13 @@ def makeFollowerVector(person, faction, columnsVector):
 
 
 
-def followerMatrix(faction, columnsVector):
+def followerMatrix(faction, columnsVector, parent):
     matrix = []
     array = []
 
-    for person in os.listdir("/Users/teaganjohnson/Desktop/TwitterAnalysis/{}".format(faction)):
+    for person in os.listdir("{}/{}".format(parent, faction)):
         if person[0] != ".":
-            followerVector = makeFollowerVector(person, faction, columnsVector)
+            followerVector = makeFollowerVector(person, faction, columnsVector, parent)
             matrix.append(followerVector)
             if faction == 'Donald Trump Followers':
                 array.append('1')
@@ -43,14 +46,25 @@ def createTop50List(path):
             top50.remove("('realDonaldTrump',)")
         return top50
 
-trumpTop50 = createTop50List("/Users/teaganjohnson/Desktop/TwitterAnalysis/trumpTop100.csv")
-bidenTop50 = createTop50List("/Users/teaganjohnson/Desktop/TwitterAnalysis/bidenTop100.csv")
+trumpTop50 = createTop50List("{}/trumpTop100.csv".format(parent))
+bidenTop50 = createTop50List("{}/bidenTop100.csv".format(parent))
 
 top100 = trumpTop50 + bidenTop50
 columnsVector = list(dict.fromkeys(top100))
 
-trumpMatrix, trumpArray = followerMatrix("Donald Trump Followers", columnsVector)
-bidenMatrix, bidenArray = followerMatrix("Joe Biden Followers", columnsVector)
-bothMatrix, bothArray = followerMatrix("Both Followers", columnsVector)
+trumpMatrix, trumpArray = followerMatrix("Donald Trump Followers", columnsVector, parent)
+bidenMatrix, bidenArray = followerMatrix("Joe Biden Followers", columnsVector, parent)
+bothMatrix, bothArray = followerMatrix("Both Followers", columnsVector, parent)
 
-print(trumpArray)
+
+matrix_list = np.concatenate((trumpMatrix, bidenMatrix), axis = 0)
+matrix = np.array(matrix_list)
+np.save('/{}/matrix.npy'.format(parent), matrix)
+
+np.save('/{}/bothMatrix.npy'.format(parent), bothMatrix)
+
+targetArray_list = np.concatenate((trumpArray, bidenArray), axis = 0)
+targetArray = np.array(targetArray_list)
+np.save('/{}/tagetArray.npy'.format(parent), targetArray)
+
+np.save('/{}/topTeamsList.npy'.format(parent), top100)
